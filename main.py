a91210c8353335c01b746275cb229ba5c9848ad0
usmonim <usmoni.muhamad2001@protonmail.com>
2022-06-22 12:42:37 +0300

    tst

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ file_name = sys.argv[1]
 action = sys.argv[2];
 
 with open(file_name, 'r+') as file:
-    data = [line.strip() for line in file] #take all data line by line.
+    data = [line.strip() for line in file] #take all data from opened file
 
 # print(data[1])
 
 #if user enter 1 in command line argument then we perform action "Добавить в список/Add to list"
 if int(action) == 1:
-#Ask user to enter what he want yo add to list
+#Ask user to enter what he want yo add to the list
     add = input("1 Добавить в список: ")
     execute = ActionOne(file_name, add)
     execute.adding()
+    print(data)
     # data.append(add)
